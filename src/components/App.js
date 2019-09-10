db930d74e1cdf8f5e0a4c7b71ef925a327a32901
albertohasegawa <albertohasegawa@gmail.com>
2019-09-10 16:31:03 -0300

    course finish

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ResourceList from './ResourceList';
+import UserList from './UserList';
 
-class App extends React.Component {
-  render() {
-    return <div>App</div>;
-  }
-}
+const App = () => {
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      <UserList />
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
